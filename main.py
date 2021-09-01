a096fd2e92552c283962dc1d851d40343db04ff5
valeriapopova <iamvaleriapopova@yandex.ru>
2021-09-01 14:35:42 +0400

    ended

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
     def rate_lectures(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
@@ -15,17 +16,22 @@ class Student:
         else:
             return 'Ошибка'
     def avg_grade(self):
-        self.len = self.grades['Python'] + self.grades['Git']
-        avg = sum(self.grades['Python'] + self.grades['Git']) / len(self.len)
+        my_list = []
+        for grade in self.grades.values():
+            for mark in grade:
+                my_list.append(mark)
+        avg = sum(my_list) / len(my_list)
         return avg
-    def __str__(self):
-        res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: {self.avg_grade()} \nКурсы в процессе изучения: {",".join(self.courses_in_progress)} \nЗавершенные курсы: {",".join(self.finished_courses)} '
-        return res
+
     def __lt__(self, other):
         if not isinstance(other, Student):
             print('Не студент')
             return
         return self.avg_grade() < other.avg_grade()
+
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: {self.avg_grade()} \nКурсы в процессе изучения: {",".join(self.courses_in_progress)} \nЗавершенные курсы: {",".join(self.finished_courses)} '
+        return res
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -41,8 +47,11 @@ class Lecturer (Mentor):
         self.courses_in_progress = []
         self.grades = {}
     def lec_avg_grade(self):
-        self.len = self.grades['Python'] + self.grades['Git']
-        lec_avg = sum(self.grades['Python'] + self.grades['Git']) / len(self.len)
+        my_list = []
+        for grade in self.grades.values():
+            for mark in grade:
+                my_list.append(mark)
+        lec_avg = sum(my_list) / len(my_list)
         return lec_avg
     def __str__(self):
         res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.lec_avg_grade()} '
@@ -85,6 +94,8 @@ bad_student.courses_in_progress += ['Git']
 worst_student = Student('Harry', 'Potter', 'gender')
 worst_student.courses_in_progress += ['Python']
 worst_student.courses_in_progress += ['Git']
+
+students = [bad_student, worst_student]
 
 some_reviewer = Reviewer('Some', 'Buddy')
 some_reviewer.courses_attached += ['Python']
@@ -130,6 +141,8 @@ lec = Lecturer('Second', 'Guy')
 lec.courses_attached += ['Python']
 lec.courses_attached += ['Git']
 
+lecturers = [lec, one_lecturer]
+
 cool_student = Student('Some', 'Buddy', 'your_gender')
 cool_student.courses_in_progress += ['Python']
 cool_student.courses_in_progress += ['Git']
@@ -158,6 +171,36 @@ cool_student.rate_lectures(lec, 'Git', 9)
 # print(bad_student)
 # print(worst_student)
 # print(best_student < worst_student)
-# print(lec < one_lecturer)
+# print(lec < some_lecturer)
 
 
+
+def avg_grade_kurs(students, course):
+    mylist = []
+    for student in students:
+        if student.grades.get(course) != None:
+            for kurs in student.grades.get(course):
+                mylist.append(kurs)
+        else:
+            pass
+    avg_grade_kurs = sum(mylist) / len(mylist)
+    return avg_grade_kurs
+
+
+avg_grade_kurs = avg_grade_kurs(students, course='Git')
+print(f'Средняя оценка по домашнему заданиюпо всем студентам курса "Git" = {avg_grade_kurs}')
+
+def lec_avg_grade_kurs(lecturers, course):
+    mylist = []
+    for lecturer in lecturers:
+        if lecturer.grades.get(course) != None:
+            for kurs in lecturer.grades.get(course):
+                mylist.append(kurs)
+        else:
+            pass
+    lec_avg_grade_kurs = sum(mylist) / len(mylist)
+    return lec_avg_grade_kurs
+
+
+lec_avg_grade_kurs = lec_avg_grade_kurs(lecturers, course='Git')
+print(f'Средняя оценка за лекции всех лекторов в рамках курса "Git" = {lec_avg_grade_kurs}')
